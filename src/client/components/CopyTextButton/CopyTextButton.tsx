5c7a836d74d2c5f239e3b88882715e482c8fe1bc
Sullivan Ford <sullyjhford@gmail.com>
2022-09-19 18:45:03 +0100

    Show copy button text as copied when copied

--- src/client/components/CopyTextButton/CopyTextButton.tsx
+++ src/client/components/CopyTextButton/CopyTextButton.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './copy-text-button.scss';
 
 export const CopyTextButton = ({ text }) => {
+  const [copied, setCopied] = useState(false);
+  const onCopyClicked = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3333);
+  };
   return (
     <div className="copy-text-wrapper">
       <div className="text">{text}</div>
-      <button
-        onClick={(e) => {
-          navigator.clipboard.writeText(text);
-        }}
-        className="copy-btn"
-      >
-        Copy
+      <button onClick={onCopyClicked} className="copy-btn">
+        {copied ? 'Copied!' : 'Copy'}
       </button>
     </div>
   );
