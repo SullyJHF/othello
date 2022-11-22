c89ad1489938a69c811557a4132c0aaa98ae020f
Sullivan Ford <sullyjhford@gmail.com>
2022-11-22 16:44:38 +0000

    Make host/join screen look better

--- src/client/components/MainMenu/MainMenu.tsx
+++ src/client/components/MainMenu/MainMenu.tsx
@@ -6,8 +6,15 @@ export const MainMenu = () => {
   // Show games that this user is in and let user click them
   return (
     <div id="main-menu">
-      <Link to="/host">Host Game</Link>
-      <Link to="/join">Join Game</Link>
+      <div className="menu-wrapper card">
+        <h1 className="title">Othello</h1>
+        <Link className="link" to="/host">
+          Host Game
+        </Link>
+        <Link className="link" to="/join">
+          Join Game
+        </Link>
+      </div>
     </div>
   );
 };
