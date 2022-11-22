033fee75e768393b27ddb8f3ef31dffd663b2339
Sullivan Ford <sullyjhford@gmail.com>
2022-11-22 16:59:25 +0000

    Improve host game screen

--- src/client/components/MainMenu/HostGameMenu.tsx
+++ src/client/components/MainMenu/HostGameMenu.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { FormEventHandler, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SocketEvents } from '../../../shared/SocketEvents';
 import { useLocalStorage } from '../../utils/hooks';
 import { useSocket } from '../../utils/socketHooks';
+import './host-game-menu.scss';
 
 export const HostGameMenu = () => {
   const { socket, localUserId } = useSocket();
   const navigate = useNavigate();
   const [userName, setUsername] = useLocalStorage('username', '');
   const [localUserName, setLocalUserName] = useState(userName);
-  const onHostClicked = () => {
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
     setUsername(localUserName);
     socket.emit(SocketEvents.HostNewGame, localUserId, localUserName, (gameId: string) => {
       console.log(`Game created, ${gameId}`);
@@ -18,13 +20,20 @@ export const HostGameMenu = () => {
   };
   return (
     <div id="host-menu">
-      <input
-        type="text"
-        placeholder="Username"
-        value={localUserName}
-        onChange={(e) => setLocalUserName(e.target.value)}
-      />
-      <button onClick={onHostClicked}>Start Game</button>
+      <div className="card host-wrapper">
+        <form onSubmit={onSubmit}>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={localUserName}
+            onChange={(e) => setLocalUserName(e.target.value)}
+          />
+          <button className="link" type="submit">
+            Start Game
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
